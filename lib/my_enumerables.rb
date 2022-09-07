79250a378584e0cb666c5eb18730e6e0e8e661ed
Nicole Leinich <nicole.leinich@ivx.com>
2022-09-07 15:17:55 +0200

    Created my_each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,17 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    result_array = Array.new
+    original_array = self.clone
+    if block_given?
+      (0...original_array.length).each do| i |
+        result_array.push(yield(original_array[i]))
+      end
+    else
+      result_array = Enumerator.new
+    end
+
+    result_array.all?{|x| x.nil?} ? original_array : result_array
+  end
 end
