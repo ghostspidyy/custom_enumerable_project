1db7e3279d554be443269640264cf9076e1430eb
Nicole Leinich <nicole.leinich@ivx.com>
2022-09-08 13:15:55 +0200

    style formatting with rubocop

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -4,29 +4,31 @@
 module Enumerable
   # Your code goes here
   def my_all?
-    original_obj = self.clone
+    original_obj = clone
     control = 0
 
-    (0...original_obj.length).each {|i| control += 1 if yield(original_obj[i])} if block_given?
+    (0...original_obj.length).each { |i| control += 1 if yield(original_obj[i]) } if block_given?
     control == original_obj.length
   end
 
   def my_any?
-    original_obj = self.clone
+    original_obj = clone
 
-    (0...original_obj.length).each {|i| return true if yield(original_obj[i]) } if block_given?
+    (0...original_obj.length).each { |i| return true if yield(original_obj[i]) } if block_given?
     false
   end
 
   def my_count
-    original_obj = self.clone
+    original_obj = clone
     return original_obj.length unless block_given?
+
     control = 0
-    (0...original_obj.length).each {|i| control +=1 if yield(original_obj[i])}
+    (0...original_obj.length).each { |i| control += 1 if yield(original_obj[i]) }
     control
   end
+
   def my_each_with_index
-    original_obj = self.clone
+    original_obj = clone
     result = original_obj.class.new
 
     if block_given?
@@ -38,10 +40,10 @@ module Enumerable
   end
 
   def my_inject(initial = nil)
-    original_obj = self.clone
+    original_obj = clone
     memo = initial.nil? ? 0 : initial
     if block_given?
-      (0...original_obj.length).each do | i |
+      (0...original_obj.length).each do |i|
         memo = yield(memo, original_obj[i])
       end
     end
@@ -50,26 +52,28 @@ module Enumerable
 
   def my_map
     return Enumerator.new unless block_given?
-    original_obj = self.clone
+
+    original_obj = clone
     result_arr = []
-    (0...original_obj.length).each { |i| result_arr.push(yield(original_obj[i]))}
+    (0...original_obj.length).each { |i| result_arr.push(yield(original_obj[i])) }
     result_arr
   end
 
   def my_none?
-    original_obj = self.clone
+    original_obj = clone
     control = 0
 
-    (0...original_obj.length).each {|i| control += 1 if yield(original_obj[i]) } if block_given?
-    control == 0
+    (0...original_obj.length).each { |i| control += 1 if yield(original_obj[i]) } if block_given?
+    control.zero?
   end
 
   def my_select
     return Enumerator.new unless block_given?
-    original_obj = self.clone
+
+    original_obj = clone
     result = original_obj.class.new
 
-    (0...original_obj.length).each {|i| result.push(original_obj[i]) if yield(original_obj[i])}
+    (0...original_obj.length).each { |i| result.push(original_obj[i]) if yield(original_obj[i]) }
     result.nil? ? original_obj : result
   end
 end
@@ -82,11 +86,12 @@ class Array
   # Define my_each here
   def my_each
     return Enumerator.new unless block_given?
-    result_array = Array.new
-    original_array = self.clone
 
-    (0...original_array.length).each {| i | result_array.push(yield(original_array[i]))}
+    result_array = []
+    original_array = clone
 
-    result_array.all?{|x| x.nil?} ? original_array : result_array
+    (0...original_array.length).each { |i| result_array.push(yield(original_array[i])) }
+
+    result_array.all?(&:nil?) ? original_array : result_array
   end
 end
